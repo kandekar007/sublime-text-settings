67371f532fb07943b2231452523f17eb0dee7feb
Aditya Kandekar <adi.rk.21b@gmail.com>
2021-07-24 01:52:34 +0530

    Update cf-round.bat

--- cf-round.bat
+++ cf-round.bat
@@ -1,10 +1,9 @@
 @echo off
 set /p round="Enter Round: "
-mkdir "%round%" 
+mkdir "%round%"
 
-echo  .> %round%/A.cpp
-echo  .> %round%/B.cpp
-echo  .> %round%/C.cpp
-echo  .> %round%/input.txt
-echo  .> %round%/output.txt
-
+echo .> "%round%/A.cpp"
+echo .> "%round%/B.cpp"
+echo .> "%round%/C.cpp"
+echo .> "%round%/input.txt"
+echo .> "%round%/output.txt"
