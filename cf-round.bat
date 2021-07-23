15881109d1c5eae772e9d1d935bc3c3a9eb86509
Aditya Kandekar <kandekar@iitg.ac.in>
2021-07-24 00:45:04 +0530

    Script to create contest folder in windows

---
+++ cf-round.bat
@@ -0,0 +1,10 @@
+@echo off
+set /p round="Enter Round: "
+mkdir %round% 
+
+echo  .> %round%/A.cpp
+echo  .> %round%/B.cpp
+echo  .> %round%/C.cpp
+echo  .> %round%/input.txt
+echo  .> %round%/output.txt
+
