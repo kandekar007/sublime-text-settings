844c847c16422ee03f37e859790f2b44538b9a05
Aditya Kandekar <kandekar@iitg.ac.in>
2021-07-24 01:54:47 +0530

    [linux] create folders for CF contest

---
+++ cf-contest.sh
@@ -0,0 +1,10 @@
+echo -n "Round Name:"
+read round
+mkdir "${round}"
+
+touch "${round}\A.cpp"
+touch "${round}\B.cpp"
+touch "${round}\C.cpp"
+touch "${round}\input.txt"
+touch "${round}\output.txt"
+
